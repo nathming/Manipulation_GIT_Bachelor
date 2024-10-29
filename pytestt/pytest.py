3b0659b67abda92cac1df8e5163f58be98ad3d80
bachelor <bachelor@DESKTOP-ASCVQPV>
2024-10-29 15:35:16 +0100

    Manipulation_GIT_Bachelor

---
+++ pytestt/pytest.py
@@ -0,0 +1,13 @@
+def pytry():
+    return(1/0)
+
+def test_pytry():
+    assert(1/0)
+
+def test_divide():
+    for i in range (-5,100):
+        assert(5/i)
+
+def test_ranging():
+    for i in range (-5,100):
+        assert(i==5 or i==10)
