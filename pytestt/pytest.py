c4007bf8a3804af2391cfb81ab34cecfed780910
bachelor <bachelor@DESKTOP-ASCVQPV>
2024-10-29 16:14:42 +0100

    ajout de docstring

--- pytestt/pytest.py
+++ pytestt/pytest.py
@@ -1,16 +1,30 @@
 def pytry():
+    '''
+    cette fonction essaie de diviser 1 par 0 
+    ce qui va causer une erreur de division par zéro.
+    '''
     return(1/0)
 
 def test_pytry():
+    '''
+    cette fonction permet de tester pytest
+    '''
     assert(1/0)
 
 def test_divide():
+    '''
+    cette fonction permet de tester pytest avec plus d'algo
+    '''
     for i in range (-5,100):
         assert(5/i)
 
 def test_ranging():
+    '''
+    cette fonction permet de tester pytest
+    '''
     for i in range (-5,100):
         assert(i==5 or i==10)
+
 
 
 #Pierre was here
